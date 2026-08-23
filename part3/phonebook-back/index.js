7ef2d06c89d08a851b93625bb82027b47586753b
mierri <marygonzalez1603@gmail.com>
2026-08-23 17:25:53 -0600

    Exercises 3.1-3.6

---
+++ part3/phonebook-back/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const persons = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const personIndex = persons.findIndex((person) => person.id === id)
+
+  if (personIndex !== -1) {
+    persons.splice(personIndex, 1)
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body
+
+  if (!name || !number) {
+    return response.status(400).json({ error: 'Name or number is missing' })
+  }
+
+  const existingPerson = persons.find((person) => person.name === name)
+  if (existingPerson) {
+    return response.status(400).json({ error: 'Name must be unique' })
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 1000000000).toString(),
+    name,
+    number,
+  }
+
+  persons.push(newPerson)
+  response.status(201).json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
