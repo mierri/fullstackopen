c86247f0f778e9cabac3800b9a6cec8995b3d236
mierri <marygonzalez1603@gmail.com>
2026-08-23 19:20:35 -0600

    Exercises 3.9-3.11 (in progress)

--- part3/phonebook-back/index.js
+++ part3/phonebook-back/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
