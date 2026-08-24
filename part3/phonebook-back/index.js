38e82b5a18aca8c131a943ff807c1d3370d983e7
mierri <marygonzalez1603@gmail.com>
2026-08-24 01:08:55 -0600

    Exercises 3.12-3.18 (corrections)

--- part3/phonebook-back/index.js
+++ part3/phonebook-back/index.js
@@ -107,6 +107,6 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
 })
